4572f592872992e5094c5956ae8dc18234e3e3ea
Sangbeom Heo <fivethreeeo@gmail.com>
2023-08-30 15:29:03 +0900

    feat: Icon 컴포넌트 생성
    - sprite svg 적용

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,21 @@
+import sprite from './sprite.svg'
+
+export type IconNameType =
+  | 'icon_arrowRight'
+  | 'icon_check'
+  | 'icon_minus'
+  | 'icon_plus'
+  | 'icon_spot'
+
+interface IconProps {
+  name: IconNameType
+  size?: 12 | 24
+}
+
+export default function Icon({ name, size = 24 }: IconProps) {
+  return (
+    <svg width={size} height={size}>
+      <use href={`${sprite}#${name}`} />
+    </svg>
+  )
+}
