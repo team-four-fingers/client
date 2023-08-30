8d9d5deaa824020a272c4aa40c1fd30ba305f80d
Sangbeom Heo <fivethreeeo@gmail.com>
2023-08-30 21:18:13 +0900

    라이브러리 사용해서 마커, 선 그리기 테스트

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,12 +1,9 @@
 import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk'
 
-// function MapSample({ marker, line }) {
-//   return ()
-// }
-
 type MarkerInfoType = {
   position: { lat: number; lng: number }
-  imageUrl: string
+  src: string
+  size: { width: number; height: number }
 }
 
 interface MarkersProps {
@@ -16,13 +13,13 @@ interface MarkersProps {
 function Markers({ markerList }: MarkersProps) {
   return (
     <>
-      {markerList.map(({ position, imageUrl }) => (
+      {markerList.map(({ position, src, size }) => (
         <MapMarker
           key={`${position.lat}-${position.lng}`}
           position={position}
           image={{
-            src: imageUrl,
-            size: { width: 24, height: 35 },
+            src,
+            size,
           }}
         />
       ))}
@@ -34,33 +31,35 @@ export default function Search() {
   const markerList = [
     {
       position: { lat: 33.55635, lng: 126.795841 },
-      imageUrl: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+      src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+      size: { width: 24, height: 35 },
     },
     {
       position: { lat: 33.557, lng: 126.795609 },
-      imageUrl: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+      src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+      size: { width: 24, height: 35 },
     },
     {
       position: { lat: 33.55755, lng: 126.795511 },
-      imageUrl: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+      src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+      size: { width: 24, height: 35 },
     },
   ]
 
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '100%', height: '100%' }}>
-      <Markers markerList={markerList} />
-      <Polyline
-        path={[
-          { lat: 33.55635, lng: 126.795841 },
-          { lat: 33.557, lng: 126.795609 },
-          { lat: 33.55755, lng: 126.795511 },
-          { lat: 33.55995, lng: 126.795599 },
-        ]}
-        strokeWeight={5} // 선의 두께 입니다
-        strokeColor={'#FFAE00'} // 선의 색깔입니다
-        strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        strokeStyle={'solid'} // 선의 스타일입니다
-      />
-    </Map>
+    <>
+      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '100%', height: '100%' }}>
+        <Markers markerList={markerList} />
+        <Polyline
+          path={[
+            { lat: 33.55635, lng: 126.795841 },
+            { lat: 33.557, lng: 126.795609 },
+            { lat: 33.55755, lng: 126.795511 },
+            { lat: 33.55995, lng: 126.795599 },
+          ]}
+        />
+      </Map>
+      <div>tap bar</div>
+    </>
   )
 }
