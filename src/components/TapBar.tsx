5239c911cead74edac9673ef46565b73a71a5945
Sangbeom Heo <fivethreeeo@gmail.com>
2023-08-30 23:34:54 +0900

    feat: TapBar 구현
    - 탐색 페이지 이외에 탭바를 사용하지 않아서 Search.tsx 만 넣음

---
+++ src/components/TapBar.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom'
+import Icon, { IconNameType } from './Icon'
+import styles from './Tapbar.module.scss'
+
+export default function TapBar() {
+  type TapBarItem = {
+    name: '탐색' | '장바구니' | '저장 경로'
+    icon: IconNameType
+    path: string
+    active: boolean
+  }
+
+  const tapBarItems: TapBarItem[] = [
+    { name: '탐색', icon: 'icon_check', path: '/', active: true },
+    { name: '장바구니', icon: 'icon_check', path: '/cart', active: false },
+    { name: '저장 경로', icon: 'icon_check', path: '/savedRoutes', active: false },
+  ]
+
+  return (
+    <div className={styles.container}>
+      {tapBarItems.map(item => (
+        <div className={item.active ? styles.activeItem : styles.item} key={item.path}>
+          <NavLink to={item.path}>
+            <Icon name={item.icon} />
+            <div className={styles.label}>{item.name}</div>
+          </NavLink>
+        </div>
+      ))}
+    </div>
+  )
+}
