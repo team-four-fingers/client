031dce753341a377140dbb3e4bd4031e87fbfded
Sangbeom Heo <fivethreeeo@gmail.com>
2023-09-01 05:41:58 +0900

    장바구니, 저장경로 페이지에서  탭바 보이게, 저장경로 콘텐츠 추가 (#19)
    * feat: 탭바 장바구니, 저장경로 탭에서도 보이게 변경
    
    * feat: 저장경로 탭 이미지  추가
    
    * design: 검색바 위치 조정

--- src/components/TapBar.tsx
+++ src/components/TapBar.tsx
@@ -1,26 +1,27 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import Icon, { IconNameType } from './Icon'
 
 import styles from './TapBar.module.scss'
 
 export default function TapBar() {
+  const { pathname } = useLocation()
+
   type TapBarItem = {
     name: '탐색' | '장바구니' | '저장 경로'
     icon: IconNameType
     path: string
-    active: boolean
   }
 
   const tapBarItems: TapBarItem[] = [
-    { name: '탐색', icon: 'icon-search-solid', path: '/', active: true },
-    { name: '장바구니', icon: 'icon-cart-line', path: '/cart', active: false },
-    { name: '저장 경로', icon: 'icon-bookmark-line', path: '/savedRoutes', active: false },
+    { name: '탐색', icon: 'icon-search-line', path: '/' },
+    { name: '장바구니', icon: 'icon-cart-line', path: '/cart' },
+    { name: '저장 경로', icon: 'icon-bookmark-line', path: '/savedRoutes' },
   ]
 
   return (
     <div className={styles.container}>
       {tapBarItems.map(item => (
-        <div className={item.active ? styles.activeItem : styles.item} key={item.path}>
+        <div className={item.path === pathname ? styles.activeItem : styles.item} key={item.path}>
           <NavLink to={item.path}>
             <Icon name={item.icon} />
             <div className={styles.label}>{item.name}</div>
