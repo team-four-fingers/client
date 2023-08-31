c773646553b5f2fc4b8ad02cec433f3c3323f507
Sangbeom Heo <fivethreeeo@gmail.com>
2023-09-01 05:39:07 +0900

    feat: 저장경로 탭 이미지  추가

--- src/pages/SavedRoutes.tsx
+++ src/pages/SavedRoutes.tsx
@@ -1,3 +1,14 @@
+import TapBar from '../components/TapBar'
+import savedRouteContentUrl from '../assets/savedRouteContent.png'
+
 export default function SavedRoutes() {
-  return <div>SavedRoutes</div>
+  return (
+    <div>
+      <div style={{ maxWidth: '390px', margin: '0 auto' }}>
+        <img src={savedRouteContentUrl} alt='' />
+      </div>
+      <div style={{ content: ' ', display: 'block', clear: 'both', height: '240px' }}></div>
+      <TapBar />
+    </div>
+  )
 }
