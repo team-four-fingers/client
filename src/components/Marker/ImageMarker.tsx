c7237580426b05366b563065165bc5ef2698916b
Sangbeom Heo <fivethreeeo@gmail.com>
2023-08-31 15:39:26 +0900

    design: 이미지 마커 그레이 크기 값 조정

--- src/components/Marker/ImageMarker.tsx
+++ src/components/Marker/ImageMarker.tsx
@@ -6,7 +6,7 @@ import markerGrayUrl from '../../assets/image-marker-gray.svg'
 const backgroundMarker = {
   blue: { src: markerBlueUrl, size: { width: 52, height: 72 } },
   orange: { src: markerOrangeUrl, size: { width: 52, height: 72 } },
-  gray: { src: markerGrayUrl, size: { width: 50, height: 50 } },
+  gray: { src: markerGrayUrl, size: { width: 50, height: 72 } },
 }
 
 type ImageMarkerProps = {
@@ -18,7 +18,7 @@ type ImageMarkerProps = {
 
 export default function ImageMarker({ position, color, imageUrl, handleClick }: ImageMarkerProps) {
   const imagePosition = { lat: position.lat, lng: position.lng }
-  const imageSize = color === 'gray' ? { width: 38, height: 50 } : { width: 38, height: 72 }
+  const imageSize = color === 'gray' ? { width: 38, height: 72 } : { width: 38, height: 72 }
   const imageInfo = { src: imageUrl, size: imageSize }
 
   return (
