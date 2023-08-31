fa7927b575aa7ca88abd5c19cb61a38d08515476
Sangbeom Heo <fivethreeeo@gmail.com>
2023-09-01 06:42:27 +0900

    feat: 장바구니에 담은 상품이 없을 때 화면 추가

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -16,6 +16,8 @@ export default function Cart() {
 
   const isAllSelected = searchResultItemsInCart.every(item => item.isSelected)
 
+  console.log(searchResultItemsInCart)
+
   return (
     <>
       <div
@@ -37,136 +39,176 @@ export default function Cart() {
         </div>
         장바구니
       </div>
-
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          padding: '16px 20px',
+          content: ' ',
+          display: 'block',
+          clear: 'both',
+          height: '1px',
+          background: '#F0F2F5',
         }}
-      >
+      ></div>
+
+      {searchResultItemsInCart.length > 0 && (
         <div
           style={{
             display: 'flex',
-            gap: '8px',
-            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: '16px 20px',
           }}
         >
-          <CheckBox
-            isChecked={isAllSelected}
-            size={20}
-            onClick={() => {
-              if (isAllSelected) {
-                unselectAllSearchResultItemsFromCart()
-              } else {
-                selectAllSearchResultItemsFromCart()
-              }
+          <div
+            style={{
+              display: 'flex',
+              gap: '8px',
+              alignItems: 'center',
             }}
-          />
-          전체 선택
+          >
+            <CheckBox
+              isChecked={isAllSelected}
+              size={20}
+              onClick={() => {
+                if (isAllSelected) {
+                  unselectAllSearchResultItemsFromCart()
+                } else {
+                  selectAllSearchResultItemsFromCart()
+                }
+              }}
+            />
+            전체 선택
+          </div>
         </div>
-      </div>
+      )}
 
-      {searchResultItemsInCart.map(item => {
-        return (
-          <>
-            <div
-              style={{
-                padding: '16px 20px',
-                color: 'var(--gray-050, #111)',
-                fontSize: '16px',
-                fontWeight: 700,
-                background: ' var(--gray-800, #F4F5F7)',
-              }}
-            >
-              {item.store.name}
-            </div>
-            <div
-              style={{
-                padding: '20px',
-                color: 'var(--gray-050, #111)',
-                fontSize: '16px',
-                fontWeight: 700,
-                display: 'flex',
-                gap: '12px',
-              }}
-            >
-              <CheckBox
-                isChecked={item.isSelected}
-                size={20}
-                onClick={() => {
-                  if (item.isSelected) {
-                    unselectSearchResultItemFromCart(item)
-                  } else {
-                    selectSearchResultItemFromCart(item)
-                  }
-                }}
-              />
-              <img
-                src={item.product.image_url}
-                width={80}
-                height={80}
-                style={{ borderRadius: '12px' }}
-              />
+      {searchResultItemsInCart.length === 0 ? (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '24px',
+            alignItems: 'center',
+            marginTop: '200px',
+          }}
+        >
+          <p style={{ color: '#9FA4B0', fontWeight: 500 }}>아직 장바구니에 담긴 상품이 없어요</p>
+          <button
+            style={{
+              width: '153px',
+              borderRadius: '12px',
+              color: '#3B7AD9',
+              fontWeight: 'bold',
+              padding: '20px 24px',
+              background: '#4A88E526',
+            }}
+            onClick={() => {
+              navigate('/')
+            }}
+          >
+            상품 찾으러 가기
+          </button>
+        </div>
+      ) : (
+        searchResultItemsInCart.map(item => {
+          return (
+            <>
               <div
                 style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '6px',
+                  padding: '16px 20px',
+                  color: 'var(--gray-050, #111)',
+                  fontSize: '16px',
+                  fontWeight: 700,
+                  background: ' var(--gray-800, #F4F5F7)',
                 }}
               >
-                <span
+                {item.store.name}
+              </div>
+              <div
+                style={{
+                  padding: '20px',
+                  color: 'var(--gray-050, #111)',
+                  fontSize: '16px',
+                  fontWeight: 700,
+                  display: 'flex',
+                  gap: '12px',
+                }}
+              >
+                <CheckBox
+                  isChecked={item.isSelected}
+                  size={20}
+                  onClick={() => {
+                    if (item.isSelected) {
+                      unselectSearchResultItemFromCart(item)
+                    } else {
+                      selectSearchResultItemFromCart(item)
+                    }
+                  }}
+                />
+                <img
+                  src={item.product.image_url}
+                  width={80}
+                  height={80}
+                  style={{ borderRadius: '12px' }}
+                />
+                <div
                   style={{
-                    color: 'var(--gray-050, #111)',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '6px',
+                  }}
+                >
+                  <span
+                    style={{
+                      color: 'var(--gray-050, #111)',
+                      fontSize: '16px',
+                      fontWeight: 700,
+                    }}
+                  >
+                    {item.product.name}
+                  </span>
+                  <span
+                    style={{
+                      color: 'var(--gray-300, #646D7A)',
+                      fontSize: '15px',
+                      fontWeight: 400,
+                    }}
+                  >
+                    {item.product.name}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                style={{
+                  padding: '20px',
+                  width: '100%',
+                  maxWidth: '640px',
+                  position: 'fixed',
+                  bottom: '84px',
+                }}
+              >
+                <button
+                  type='button'
+                  style={{
+                    padding: '16px 24px',
+                    width: '100%',
+                    borderRadius: '12px',
+                    background: 'var(--blue-100, #4A88E5)',
+                    color: 'var(--white-900, #FFF)',
                     fontSize: '16px',
                     fontWeight: 700,
                   }}
-                >
-                  {item.product.name}
-                </span>
-                <span
-                  style={{
-                    color: 'var(--gray-300, #646D7A)',
-                    fontSize: '15px',
-                    fontWeight: 400,
+                  onClick={() => {
+                    navigate('/bestRoute')
                   }}
                 >
-                  {item.product.name}
-                </span>
+                  선택한 {searchResultItemsInCart.filter(item => item.isSelected).length}개 상품
+                  사러가는 길 찾기
+                </button>
               </div>
-            </div>
-
-            <div
-              style={{
-                padding: '20px',
-                width: '100%',
-                maxWidth: '640px',
-                position: 'fixed',
-                bottom: '84px',
-              }}
-            >
-              <button
-                type='button'
-                style={{
-                  padding: '16px 24px',
-                  width: '100%',
-                  borderRadius: '12px',
-                  background: 'var(--blue-100, #4A88E5)',
-                  color: 'var(--white-900, #FFF)',
-                  fontSize: '16px',
-                  fontWeight: 700,
-                }}
-                onClick={() => {
-                  navigate('/bestRoute')
-                }}
-              >
-                선택한 {searchResultItemsInCart.filter(item => item.isSelected).length}개 상품
-                사러가는 길 찾기
-              </button>
-            </div>
-          </>
-        )
-      })}
+            </>
+          )
+        })
+      )}
       <div style={{ content: ' ', display: 'block', clear: 'both', height: '240px' }}></div>
       <TapBar />
     </>
