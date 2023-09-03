ecc3f27ccac8c56294bfd51c80ea441c6547ec5d
Sangbeom Heo <fivethreeeo@gmail.com>
2023-09-03 21:43:33 +0900

    feat: 현재 위치 주소로 표기

--- src/components/CurrentLocationBanner.tsx
+++ src/components/CurrentLocationBanner.tsx
@@ -2,7 +2,6 @@ import Icon from './Icon'
 import styles from './CurrentLocationBanner.module.scss'
 
 export default function CurrentLocationBanner({ address }: { address: string }) {
-  console.log(address)
   return (
     <div className={styles.container}>
       <Icon name='icon-spot-white' />
@@ -12,7 +11,7 @@ export default function CurrentLocationBanner({ address }: { address: string }) 
             <span>내 위치</span>를 기준으로 찾아볼게요
           </strong>
         </p>
-        <p className={styles.addr}>경기도 성남시 분당구 판교역로 152</p>
+        <p className={styles.addr}>{address}</p>
       </div>
     </div>
   )
